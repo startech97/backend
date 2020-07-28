2f333828c1ce96f3d2104f3c51212313cd6f59d9
Dima <sdi97stz97@gmail.com>
2020-07-28 14:53:07 +0300

    simpleHTTP

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-// выйти в корень приложения cd ..  зайти в папку сd имя папки
-// ctrl + c остановить процесс
-// localhost:3000
+
 const http = require('http');
 
-//мы сами создаем и настраиваем сервер
-const server = http.createServer((req, res) => {  //передаем функию которая будет выполнена когда будут определенные запросы на сервер
- // эта функция называется хендлер принимает два параметра реквест и респонз
- console.log(req.url);
- res.write('<h1>hello from nodejs</h1>');
- res.write('<h2>hello from nodejs</h2>'); 
- res.write('<h3>hello from nodejs</h3>');  //и ответ нужно закрыть
- res.end(
-     `<div style="background: red; width: 200px; height: 200px;">
-       <h1>Test</h1>
-     </div>`
- ); // в энд мы може передавать html
 
+const server = http.createServer((req, res) => { 
+    if(req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html' //мы преедаем какой то текст в формате html
+        });//первое значвние статус ответа а второй объект  ключ значение хедеров
+        res.end(`
+        <h1>form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text"/>
+                <button type="submit">Send</button>
+        </form>
+        `);
+    } else if(req.method === 'POST') {
+        const body = [];
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+
+
+        req.on('data', data => {
+            body.push(Buffer.from(data));
+        });
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1];
+            
+            res.end(`
+        <h1>ваше сообщение: ${message}</h1>
+        `);
+        });
+
+        
+    }
 });
 
-//чтобы запусть сервер
 server.listen(3000, () => {
  console.log("server is running");
-}); //первый парметр порт на котором запускаем  второй параметр коллбэк которая сработает когда сервекр запустится
+});
