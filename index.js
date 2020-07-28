59d247bf725e4b6a2c1cad9ffd55e7af26a34b1c
Dima <sdi97stz97@gmail.com>
2020-07-28 17:37:00 +0300

    11

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
+//npm run а не start
 
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 
 const server = http.createServer((req, res) => { 
     if(req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html' //мы преедаем какой то текст в формате html
-        });//первое значвние статус ответа а второй объект  ключ значение хедеров
-        res.end(`
-        <h1>form</h1>
-            <form method="post" action="/">
-                <input name="title" type="text"/>
-                <button type="submit">Send</button>
-        </form>
-        `);
+            'Content-Type': 'text/html; cahrset=utf-8'
+        });
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'),
+            'utf-8',
+            (err,content) => {
+                if(err) {throw err}
+                res.end(content);
+            }            
+            );
+        } else if(req.url === '/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'),
+            'utf-8',
+            (err,content) => {
+                if(err) {
+                    throw err;
+                }
+                res.end(content);
+            }           
+            )
+        }
+
     } else if(req.method === 'POST') {
         const body = [];
         res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
+            'Content-Type': 'text/html; charset=utf-8' // записали в хедер кодировку
         });
 
 
@@ -27,13 +43,10 @@ const server = http.createServer((req, res) => {
 
         req.on('end', () => {
             const message = body.toString().split('=')[1];
-            
             res.end(`
         <h1>ваше сообщение: ${message}</h1>
         `);
-        });
-
-        
+        });        
     }
 });
 
